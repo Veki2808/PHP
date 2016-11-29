92b3e0b9132320e5bf0896f6cef726c49d7af87d
Vernes Halilovic <verneshalilovic21@gmail.com>
2016-11-29 19:48:05 +0100

    Logic operators

--- index.php
+++ index.php
@@ -1,13 +1,15 @@
 <?php
 
-    $a = 6;
-    $b = 3;
+    $a = "5";
+    $b = 5;
 
-    if  ($a == $b) {
-        echo "Numbers are equal";
-    } else if ($a > $b) {
-        echo "First number is bigger ";
+    if  ($a === $b) { # == (equal),!= (not equal), <> (not equal), !== (notidentical)
+                                # ===(identical) provjera da li su tipovi varijabli jednaki, u ovom slučaju
+                                # $a je string i nije jednak $b integer-u
+        echo "Equal";
     }
     else {
-        echo "Second number is bigger";
+        echo "Not Equal";
     }
+
+?>
